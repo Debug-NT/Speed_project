1433c53b138d2c55f41e480d7644b1e571418ffc
Rodion <happyrodion@gmail.com>
2023-12-12 22:33:50 +0300

    Background only

---
+++ speed_project.py
@@ -0,0 +1,28 @@
+import pygame
+
+size = width, height = 1920, 1080
+
+
+def lobbi(screen):
+    clock = pygame.time.Clock()
+    background_image = pygame.image.load('background.png')
+
+    while True:
+        screen.blit(background_image, (0, 0))
+        pygame.display.update()
+        clock.tick(60)
+
+
+if __name__ == "__main__":
+    pygame.init()
+
+    size = width, height
+    screen = pygame.display.set_mode(size)
+
+    lobbi(screen)
+    pygame.display.flip()
+
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+
+    pygame.quit()
