0d8cd6f6b6c4c16f8ff44eecf1197d8133d3eb48
Rodion <happyrodion@gmail.com>
2023-12-12 23:13:44 +0300

    подсказка по картинке

--- speed_project.py
+++ speed_project.py
@@ -5,7 +5,7 @@ size = width, height = 1920, 1080
 
 def lobbi(screen):
     clock = pygame.time.Clock()
-    background_image = pygame.image.load('background.png')
+    background_image = pygame.image.load('background.png') #каринку закинуть в папку и проектом
 
     screen.blit(background_image, (0, 0))
     pygame.display.update()
